0d965296a4350ffab994e5e26af3b5e1607a14d9
Imani Ward <idward712@gmail.com>
2022-09-14 13:12:16 -0400

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by 9i on 9/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
