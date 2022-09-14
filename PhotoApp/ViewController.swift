dacd9265b794a3d7949ef72010f61d2b9f83f568
Imani Ward <idward712@gmail.com>
2022-09-14 14:18:55 -0400

    o

--- PhotoApp/ViewController.swift
+++ PhotoApp/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var greetLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
